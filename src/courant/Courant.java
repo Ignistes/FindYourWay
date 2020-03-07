9e32278ea33ad9285a3a9a794e164fa71722813e
joelm <joelm@192.168.1.10>
2020-03-07 22:14:32 +0100

    Ouverture du .nc

--- src/courant/Courant.java
+++ src/courant/Courant.java
@@ -1,5 +1,23 @@
 package courant;
 
+import java.io.IOException;
+import ucar.nc2.dataset.NetcdfDataset;
+
 public class Courant {
 
+	public static void main(String[] args) {
+		  String filename = "http://thredds.ucar.edu/thredds/dodsC/model/NCEP/GFS/CONUS_80km/GFS_CONUS_80km_20061019_0000.grib1";
+		  NetcdfDataset ncd = null;
+		  try {
+		    ncd = NetcdfDataset.openDataset(filename);
+		  } catch (IOException ioe) {
+			  
+		  } finally { 
+		    if (null != ncd) try {
+		      ncd.close();
+		    } catch (IOException ioe) {
+		    	
+		    }
+		 }
+	}
 }
