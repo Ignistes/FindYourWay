b6f738b0936ecf4b7f29efd9c610339e76cf9996
Alexandre <61246537+Ignistes@users.noreply.github.com>
2020-03-07 13:58:16 +0100

    Add files via upload

---
+++ src/courant/Courant.java
@@ -0,0 +1,5 @@
+package courant;
+
+public class Courant {
+
+}
---
+++ src/traitement/Traitement.java
@@ -0,0 +1,10 @@
+package traitement;
+
+public class Traitement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
